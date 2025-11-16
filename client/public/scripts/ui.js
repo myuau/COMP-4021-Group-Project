c392f923cb6f2d7a99625888fc7939a5eb81dc10
Cherry Lai <clai@optix.com.hk>
2025-11-16 17:58:12 +0800

    add pairup page

--- client/public/scripts/ui.js
+++ client/public/scripts/ui.js
@@ -17,6 +17,7 @@ const FrontPage = (function(){
     
     const hide = function(){
         $("#start-btn").fadeOut(500);
+        $("#front-bg.title").fadeOut(500);
     }
 
     return {initialize, show, hide};
@@ -120,6 +121,14 @@ const RegisterForm = (function(){
 const InstructionPage = (function(){
     const initialize = function(){
         $(".front.instruction").hide();
+
+        $(".submit-btn.next").click((e) => {
+            e.preventDefault();
+            FrontPageAudio.playBtnAudio();
+
+            hide();
+            PairupPage.show();
+        });
     }
 
     const show = function(){
@@ -158,14 +167,108 @@ const Toast = (function(){
     return {initialize, show, hide};
 })();
 
+const PairupPage = (function(){
+    let findingTimer;
+    let findingCount = 0;
+    let timeCount = 4;
+
+    const initialize = function(){
+        $("#pairup-bg-filter").hide();
+        $(".front.pairup").hide();
+        $("#waiting-container").hide();
+    };
+
+    const show = function(){
+        $("#pairup-bg-filter").fadeIn(500);
+        $(".front.pairup").show();
+        showFinding();
+    };
+
+    const hide = function(){
+        $("#pairup-bg-filter").hide();
+        $(".front.pairup").hide();
+    };
+
+    const showFinding = function(){
+        $("#pairup-header.finding").fadeIn(500);
+        $("#waiting-container").fadeIn(500);
+
+        if(findingTimer){
+            clearTimeout(findingTimer);
+            findingTimer = null;
+        }
+
+        function animateDots() {
+            findingCount = findingCount % 3 + 1;
+            const dots = ".".repeat(findingCount);
+            $("#pairup-header.finding span").text(dots);
+            
+            findingTimer = setTimeout(animateDots, 1000);
+        }
+
+        animateDots();
+    }
+
+    const hideFinding = function(){
+        $("#pairup-header.finding").hide();
+        $("#waiting-container").hide();
+
+        if(findingTimer){
+            clearTimeout(findingTimer);
+            findingTimer = null;
+        }
+    }
+
+    const showMatched = function(){
+        hideFinding();
+        let header = $("#pairup-header");
+        if(header.hasClass("finding")){
+            header.removeClass("finding");
+        }
+        header.addClass("match");
+        header.text("Match found!<br>Get ready to cook!");
+        $("#pairup-header").show();
+
+        setTimeout(hideMatched, 1500)
+    }
+
+    const hideMatched = function(){
+        $("#front-header.match").fadeOut(500);
+    }
+
+    const showCountdown = function(){
+        $("#countdown").fadeIn(500);
+        function countdown(){
+            timeCount--;
+            if(timeCount > 0){
+                $("#countdown").text(timeCount);
+                setTimeout(countdown, 1000);
+            }
+            else{
+                $("#countdown").text("Start!");
+                setTimeout(UI.hideFront, 1000);
+            }
+        }
+        countdown();
+    }
+
+    return { initialize, show, hide, showFinding, hideFinding, showMatched, showCountdown };
+})();
+
 const UI = (function(){
-    const components = [LoginForm, RegisterForm, FrontPage, Toast, InstructionPage];
+    const frontComponents = [LoginForm, RegisterForm, FrontPage, Toast, InstructionPage, PairupPage];
 
     const initialize = function() {
-        for (const component of components) {
+        for (const component of frontComponents) {
             component.initialize();
         }
     };
 
-    return { initialize };
+    const hideFront = function(){
+        for(const component of frontComponents){
+            component.hide();
+        }
+    }
+
+    return { initialize, hideFront };
 })();
